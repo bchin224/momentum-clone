e3358587541ee86ed9ac2f14fde448caad5e4ecf
bchin224 <bchin224@gmail.com>
2021-01-24 20:21:56 -0500

    Random photo for background - Connected to unsplash api for random photo display

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 This is a clone of the Google Chrome extension "Momentum" that generates a new photo and quote with every page visit as well as displays the time weather of a given zipcode. It also greets the user, as every home app should!
 
 ## Additional Links
-[Unsplash API](https://unsplash.com/)
-[Open Weather API](http://api.openweathermap.org)
-[Forismatic API](https://forismatic.com/en/api/)
-[Moment](http://momentjs.com/)
+- [Unsplash API](https://unsplash.com/)
+- [Open Weather API](http://api.openweathermap.org)
+- [Forismatic API](https://forismatic.com/en/api/)
+- [Moment](http://momentjs.com/)
 
 ## Planning Story
 - Set up React app template
---
+++ src/components/Layout.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+// import axios for making http requests
+import axios from 'axios'
+
+// api url + random photo url GET extension + client id
+const apiUrl = 'https://api.unsplash.com/photos/random/   ?client_id=rhy5w5kA-6vOu0XFIEHga_1ceg39oh5ozZ8sIdEys04'
+
+// REMOVE SPACE AFTER RANDOM TO MAKE PHOTO ACTUALLY SHOW UP
+
+class ShowPhoto extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      photo: null
+    }
+  }
+
+  componentDidMount () {
+    return axios({
+      url: apiUrl,
+      method: 'GET'
+    })
+    .then(res => this.setState({ pictureUrl: res.data.urls.regular }))
+    .catch('Error loading background', console.error)
+  }
+
+  // render photo
+  render () {
+    const { pictureUrl } = this.state
+    let backgroundPhoto = <img src={pictureUrl} alt='random'/>
+
+    return (
+      <div id="background-photo">
+        {backgroundPhoto}
+      </div>
+    )
+  }
+}
+
+export default ShowPhoto
